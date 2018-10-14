fc400e299e85a7310dcc90abce2c63d075cacdd5
jumpei-kaizu-recochoku <jumpei.kaizu@recochoku.co.jp>
2018-10-14 23:37:54 +0900

    記事の詳細画面作成

--- app/src/main/java/sample/qiitaclient/MainActivity.kt
+++ app/src/main/java/sample/qiitaclient/MainActivity.kt
@@ -18,11 +18,15 @@ class MainActivity : AppCompatActivity() {
 
         val listView: ListView = findViewById(R.id.list_view) as ListView
         listView.adapter = listAdapter
+        listView.setOnItemClickListener { adapterView, view, position, id ->
+            val article: Article = listAdapter.articles[position]
+            ArticleActivity.intent(this, article).let { startActivity(it) }
+        }
     }
 
     private fun dummyArticle(title: String, userName: String): Article =
             Article(id = "",
                     title = title,
-                    url = "https://kotlinlang.org/",
+                    url = "https://wizy.jp/",
                     user = User(id = "", name = userName, profileImageUrl = ""))
 }
