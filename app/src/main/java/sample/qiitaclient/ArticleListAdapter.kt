09b96eb5cd620bb7da14e8cb56aa7693cdb8dc41
rawsk <raws.0705.k@gmail.com>
2018-10-14 18:51:29 +0900

    記事のリスト化

--- app/src/main/java/sample/qiitaclient/ArticleListAdapter.kt
+++ app/src/main/java/sample/qiitaclient/ArticleListAdapter.kt
@@ -7,5 +7,20 @@ import android.widget.BaseAdapter
 import sample.qiitaclient.model.Article
 import sample.qiitaclient.view.ArticleView
 
-class ArticleListAdapter {
+class ArticleListAdapter(private val context: Context) : BaseAdapter() {
+
+    var articles: List<Article> = emptyList()
+
+    override fun getCount(): Int = articles.size
+
+    override fun getItem(position: Int): Any? = articles[position]
+
+    override fun getItemId(position: Int): Long = 0
+
+    override fun getView(position: Int,
+                         convertView: View?,
+                         parent: ViewGroup?): View =
+            ((convertView as? ArticleView) ?: ArticleView(context)).apply {
+                setArticle(articles[position])
+            }
 }
--- app/src/main/java/sample/qiitaclient/MainActivity.kt
+++ app/src/main/java/sample/qiitaclient/MainActivity.kt
@@ -1,24 +1,28 @@
 package sample.qiitaclient
 
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ListView
 import sample.qiitaclient.model.Article
 import sample.qiitaclient.model.User
-import sample.qiitaclient.view.ArticleView
-
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        val articleView = ArticleView(applicationContext)
+        val listAdapter = ArticleListAdapter(applicationContext)
+        listAdapter.articles = listOf(dummyArticle("Kotlin入門", "ほげ"),
+                dummyArticle("java", "fuga"))
 
-        articleView.setArticle(Article(id = "123",
-                title = "Kotlin",
-                url = "http://www.example.com/articles/123",
-                user = User(id = "456", name = "ほげ", profileImageUrl = "http://www.example.com/img")))
-
-        setContentView(articleView)
+        val listView: ListView = findViewById(R.id.list_view) as ListView
+        listView.adapter = listAdapter
     }
+
+    private fun dummyArticle(title: String, userName: String): Article =
+            Article(id = "",
+                    title = title,
+                    url = "https://kotlinlang.org/",
+                    user = User(id = "", name = userName, profileImageUrl = ""))
 }
