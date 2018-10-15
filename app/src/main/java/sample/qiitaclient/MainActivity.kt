c63b69a30b5350c7bd6f2926ad371578431718c3
jumpei-kaizu-recochoku <jumpei.kaizu@recochoku.co.jp>
2018-10-15 20:41:10 +0900

    インジケータの表示

--- app/src/main/java/sample/qiitaclient/MainActivity.kt
+++ app/src/main/java/sample/qiitaclient/MainActivity.kt
@@ -1,13 +1,16 @@
 package sample.qiitaclient
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
+import android.widget.ProgressBar
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.trello.rxlifecycle.components.support.RxAppCompatActivity
+import com.trello.rxlifecycle.kotlin.bindToLifecycle
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -17,21 +20,22 @@ import sample.qiitaclient.client.ArticleClient
 import sample.qiitaclient.model.Article
 import sample.qiitaclient.model.User
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : RxAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val listAdapter = ArticleListAdapter(applicationContext)
-        listAdapter.articles = listOf(dummyArticle("Kotlin入門", "ほげ"),
-                dummyArticle("java", "fuga"))
-
         val listView: ListView = findViewById(R.id.list_view) as ListView
+        val progressBar: ProgressBar = findViewById(R.id.progress_bar) as ProgressBar
+        val queryEditText: EditText = findViewById(R.id.query_edit_text) as EditText
+        val searchButton: Button = findViewById(R.id.search_button) as Button
+
+        val listAdapter = ArticleListAdapter(applicationContext)
         listView.adapter = listAdapter
         listView.setOnItemClickListener { adapterView, view, position, id ->
-            val article: Article = listAdapter.articles[position]
-            ArticleActivity.intent(this, article).let { startActivity(it) }
+            val intent = ArticleActivity.intent(this, listAdapter.articles[position])
+            startActivity(intent)
         }
 
         val gson: Gson = GsonBuilder()
@@ -44,13 +48,16 @@ class MainActivity : AppCompatActivity() {
                 .build()
         val articleClient: ArticleClient = retrofit.create(ArticleClient::class.java)
 
-        val queryEditText: EditText = findViewById(R.id.query_edit_text) as EditText
-        val searchButton: Button = findViewById(R.id.search_button) as Button
-
         searchButton.setOnClickListener {
+            progressBar.visibility = View.VISIBLE
+
             articleClient.search(queryEditText.text.toString())
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
+                    .doAfterTerminate {
+                        progressBar.visibility = View.GONE
+                    }
+                    .bindToLifecycle(this)
                     .subscribe({
                         queryEditText.text.clear()
                         listAdapter.articles = it
@@ -60,10 +67,4 @@ class MainActivity : AppCompatActivity() {
                     })
         }
     }
-
-    private fun dummyArticle(title: String, userName: String): Article =
-            Article(id = "",
-                    title = title,
-                    url = "https://wizy.jp/",
-                    user = User(id = "", name = userName, profileImageUrl = ""))
 }
