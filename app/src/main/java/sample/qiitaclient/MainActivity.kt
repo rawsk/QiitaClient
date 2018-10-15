e3da2942bce0b2ea52f8bf55daac0543c5733602
jumpei-kaizu-recochoku <jumpei.kaizu@recochoku.co.jp>
2018-10-15 20:22:05 +0900

    Qiita APIクライアント設定

--- app/src/main/java/sample/qiitaclient/MainActivity.kt
+++ app/src/main/java/sample/qiitaclient/MainActivity.kt
@@ -2,7 +2,18 @@ package sample.qiitaclient
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ListView
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+import sample.qiitaclient.client.ArticleClient
 import sample.qiitaclient.model.Article
 import sample.qiitaclient.model.User
 
@@ -21,6 +32,32 @@ class MainActivity : AppCompatActivity() {
         listView.setOnItemClickListener { adapterView, view, position, id ->
             val article: Article = listAdapter.articles[position]
             ArticleActivity.intent(this, article).let { startActivity(it) }
+        }
+
+        val gson: Gson = GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .create()
+        val retrofit: Retrofit = Retrofit.Builder()
+                .baseUrl("https://qiita.com")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build()
+        val articleClient: ArticleClient = retrofit.create(ArticleClient::class.java)
+
+        val queryEditText: EditText = findViewById(R.id.query_edit_text) as EditText
+        val searchButton: Button = findViewById(R.id.search_button) as Button
+
+        searchButton.setOnClickListener {
+            articleClient.search(queryEditText.text.toString())
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({
+                        queryEditText.text.clear()
+                        listAdapter.articles = it
+                        listAdapter.notifyDataSetChanged()
+                    }, {
+                        toast("エラー: $it")
+                    })
         }
     }
 
--- app/src/main/java/sample/qiitaclient/view/ArticleView.kt
+++ app/src/main/java/sample/qiitaclient/view/ArticleView.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import sample.qiitaclient.R
 import sample.qiitaclient.bindView
 import sample.qiitaclient.model.Article
@@ -40,7 +41,6 @@ class ArticleView : FrameLayout {
     fun setArticle(article: Article) {
         titleTextView.text = article.title
         userNameTextView.text = article.user.name
-
-        profileImageView.setBackgroundColor(Color.BLACK)
+        Glide.with(context).load(article.user.profileImageUrl).into(profileImageView)
     }
 }
